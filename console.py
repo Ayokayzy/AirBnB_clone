3e5c317d87d63c91deb41caddd144eee429f09ce
Ayokayzy <theayokayzy1@gmail.com>
2024-01-11 22:23:23 +0000

    Console 0.0.1

--- console.py
+++ console.py
@@ -9,13 +9,22 @@ from models.base_model import BaseModel
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
+    def emptyline(self):
+        return
+
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
 
+    def help_quit(self):
+        print("Quit command to exit the program\n")
+
     def do_EOF(self, line):
         """EOF command to exit the program"""
         return True
+
+    def help_EOF(self):
+        print("EOF command to exit the program\n")
 
 
 if __name__ == '__main__':
